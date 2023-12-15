b02a048f2a22a3db519a323b43232ae57c733be3
ryan-karp <100547738+ryan-karp@users.noreply.github.com>
2023-12-15 10:11:04 -0500

    Updated default scoring values

--- R/scoring_rules.R
+++ R/scoring_rules.R
@@ -5,7 +5,7 @@
 scoring <- list(
   pass = list(
     pass_att = 0, pass_comp = 0, pass_inc = 0, pass_yds = 0.04, pass_tds = 4,
-    pass_int = -3, pass_40_yds = 0,  pass_300_yds = 0, pass_350_yds = 0,
+    pass_int = -2, pass_40_yds = 0,  pass_300_yds = 0, pass_350_yds = 0,
     pass_400_yds = 0
   ),
   rush = list(
@@ -14,17 +14,17 @@ scoring <- list(
     rush_100_yds = 0, rush_150_yds = 0, rush_200_yds = 0),
   rec = list(
     all_pos = TRUE,
-    rec = 0, rec_yds = 0.1, rec_tds = 6, rec_40_yds = 0, rec_100_yds = 0,
+    rec = 1, rec_yds = 0.1, rec_tds = 6, rec_40_yds = 0, rec_100_yds = 0,
     rec_150_yds = 0, rec_200_yds = 0
   ),
   misc = list(
     all_pos = TRUE,
-    fumbles_lost = -3, fumbles_total = 0,
+    fumbles_lost = -2, fumbles_total = 0,
     sacks = 0, two_pts = 2
   ),
   kick = list(
     xp = 1.0, fg_0019 = 3.0,  fg_2029 = 3.0, fg_3039 = 3.0, fg_4049 = 4.0,
-    fg_50 = 5.0,  fg_miss = 0.0
+    fg_5059 = 5.0,  fg_60 = 6.0,  fg_miss = -1.0
   ),
   ret = list(
     all_pos = TRUE,
@@ -37,14 +37,17 @@ scoring <- list(
   ),
   dst = list(
     dst_fum_rec = 2,  dst_int = 2, dst_safety = 2, dst_sacks = 1, dst_td = 6,
-    dst_blk = 1.5, dst_ret_yds = 0, dst_pts_allowed = 0
+    dst_blk = 2, dst_ret_yds = 0, dst_pts_allowed = 0
   ),
   pts_bracket = list(
-    list(threshold = 0, points = 10),
-    list(threshold = 6, points = 7),
-    list(threshold = 20, points = 4),
-    list(threshold = 34, points = 0),
-    list(threshold = 99, points = -4)
+    list(threshold = 0, points = 5),
+    list(threshold = 6, points = 4),
+    list(threshold = 13, points = 3),
+    list(threshold = 17, points = 1),
+    list(threshold = 27, points = 0),
+    list(threshold = 34, points = -1),
+    list(threshold = 45, points = -3),
+    list(threshold = 99, points = -5)
   ),
     yds_bracket = list(
     list(threshold = 99, points = 5),
@@ -111,7 +114,7 @@ scoring_empty = list(pass = list(pass_att = NULL, pass_comp = NULL, pass_inc = N
                                  WR = list(fumbles_lost = NULL, fumbles_total = NULL, sacks = NULL, two_pts = NULL),
                                  TE = list(fumbles_lost = NULL, fumbles_total = NULL, sacks = NULL, two_pts = NULL)),
                      kick = list(xp = NULL, fg_0019 = NULL, fg_2029 = NULL, fg_3039 = NULL,
-                                 fg_4049 = NULL, fg_50 = NULL, fg_miss = NULL),
+                                 fg_4049 = NULL, fg_5059 = NULL, fg_60 = NULL, fg_miss = NULL),
                      ret = list(all_pos = NULL, return_tds = NULL, return_yds = NULL),
                      idp = list(all_pos = NULL, idp_solo = NULL, idp_asst = NULL, idp_sack = NULL,
                                 idp_int = NULL, idp_fum_force = NULL, idp_fum_rec = NULL, idp_pd = NULL,
@@ -141,7 +144,7 @@ scoring_type_for_cols = c(
   rec_200_yds = "rec", all_pos = "misc", fumbles_lost = "misc",
   fumbles_total = "misc", sacks = "misc", two_pts = "misc", xp = "kick",
   fg_0019 = "kick", fg_2029 = "kick", fg_3039 = "kick", fg_4049 = "kick",
-  fg_50 = "kick", fg_miss = "kick", all_pos = "ret", return_tds = "ret",
+  fg_5059 = "kick", fg_60 = "kick", fg_miss = "kick", all_pos = "ret", return_tds = "ret",
   return_yds = "ret", all_pos = "idp", idp_solo = "idp", idp_asst = "idp",
   idp_sack = "idp", idp_int = "idp", idp_fum_force = "idp", idp_fum_rec = "idp",
   idp_pd = "idp", idp_td = "idp", idp_safety = "idp", dst_fum_rec = "dst",
